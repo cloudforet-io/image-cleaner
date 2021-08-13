b2561ca1c3b8ffdeb2c61a953848ce38c58dfffc
GeunjeLee <glee@mz.co.kr>
2021-08-13 21:17:05 +0900

    refactor: adjust page size for fetching tags (#6)
    * refactor: adjust page size for fetching tags
    
    * refactor: Reflect review feedback
    
    * refactor: update def _get_image_tags

--- src/connector/dockerhub_connector.py
+++ src/connector/dockerhub_connector.py
@@ -1,6 +1,7 @@
 from . import BaseConnetor
 import requests
 import json
+import math
 
 class DockerHubConnector(BaseConnetor):
 
@@ -9,6 +10,29 @@ class DockerHubConnector(BaseConnetor):
         self.base_url = self.config['options']['url']
         self.organization = self.config['options']['organization']
         self.login()
+
+    def _http_requests(self, url, *, method, payload=None, headers=None):
+         if method == 'post' and payload is None:
+              raise Exception('post method requires a payload')
+
+         if method in ['get', 'delete'] and headers is None:
+              raise Exception(f'{method} method requires a headers')
+
+         try:
+              if method == "post":
+                   response = requests.post(url, data=payload).json()
+              elif method == "get":
+                   response = requests.get(url, headers=headers).json()
+              elif method == "delete":
+                   response = requests.delete(url, headers=headers)
+         except requests.exceptions.ConnectionError as e:
+              raise Exception(f'Connection Error {e.response}')
+         except requests.exceptions.HTTPError as e:
+              raise Exception(f'HTTP Error {e.response}')
+         except json.JSONDecodeError as e:
+              raise Exception(f'Json Decode Error {e}')
+
+         return response
 
     def login(self):
         self.username = self.config['options']['credentials']['username']
@@ -19,15 +43,7 @@ class DockerHubConnector(BaseConnetor):
             'username': self.username,
             'password': self.password
         }
-
-        try:
-            response = requests.post(url, data=payload).json()
-        except requests.exceptions.ConnectionError as e:
-            raise Exception(f'Connection Error {e.response}')
-        except requests.exceptions.HTTPError as e:
-            raise Exception(f'HTTP Error {e.response}')
-        except json.JSONDecodeError as e:
-            raise Exception(f'Json Decode Error {e}')
+        response = self._http_requests(url, method='post', payload=payload)
 
         if response.get('token'):
             self.token = response['token']
@@ -39,15 +55,7 @@ class DockerHubConnector(BaseConnetor):
         headers = {
             'Authorization': 'JWT ' + self.token
         }
-
-        try:
-            response = requests.get(url, headers=headers).json()
-        except requests.exceptions.ConnectionError as e:
-            raise Exception(f'Connection Error {e.response}')
-        except requests.exceptions.HTTPError as e:
-            raise Exception(f'HTTP Error {e.response}')
-        except json.JSONDecodeError as e:
-            raise Exception(f'Json Decode Error {e}')
+        response = self._http_requests(url, method='get', headers=headers)
 
         if response.get('results'):
             results = response['results']
@@ -60,50 +68,55 @@ class DockerHubConnector(BaseConnetor):
 
         return images
 
-    def list_old_tags(self, image):
-        url = f'{self.base_url}/v2/repositories/{self.organization}/{image}/tags/?page_size=200'
+    def _get_image_tags(self, image, page=1, page_size=100):
+        url = f'{self.base_url}/v2/repositories/{self.organization}/{image}/tags/?page_size={page_size}&page={page}'
         headers = {
             'Authorization': 'JWT ' + self.token
         }
+        response = self._http_requests(url, method='get', headers=headers)
 
-        try:
-            response = requests.get(url, headers=headers).json()
-        except requests.exceptions.ConnectionError as e:
-            raise Exception(f'Connection Error {e.response}')
-        except requests.exceptions.HTTPError as e:
-            raise Exception(f'HTTP Error {e.response}')
-        except json.JSONDecodeError as e:
-            raise Exception(f'Json Decode Error {e}')
-
+        tags = []
         if response.get('results'):
-            results = response['results']
-        else:
+            for result in response['results']:
+                tag = {}
+                tag['name'] = result['name']
+                tag['tag_last_pushed'] = result['tag_last_pushed']
+                tags.append(tag)
+        
+        return tags
+
+    def list_old_tags(self, image):
+        url = f'{self.base_url}/v2/repositories/{self.organization}/{image}/tags/?page_size=1'
+        headers = {
+            'Authorization': 'JWT ' + self.token
+        }
+        response = self._http_requests(url, method='get', headers=headers)
+        tag_count = response.get('count')
+        if tag_count < 1:
             return []
 
-        tags = self._filter(image,results)
-        if tags:
-            print(f"old tags : {image}:{tags}")
+        pages = math.ceil(tag_count / 100)
+        tags = []
+        for page in range(1, pages + 1):
+            tags += self._get_image_tags(image,page)
 
-        return tags
+        if not tags:
+            return []
+
+        old_tags = self._filter(image, tags)
+        if old_tags:
+            print(f"old tags : {image}:{old_tags}")
+
+        return old_tags
 
     def delete(self, image, tag):
         url = f'{self.base_url}/v2/repositories/{self.organization}/{image}/tags/{tag}/'
         headers = {
             'Authorization': 'JWT ' + self.token
         }
-
-        try:
-            response = requests.delete(url, headers=headers)
-        except requests.exceptions.ConnectionError as e:
-            raise Exception(f'Connection Error {e.response}')
-        except requests.exceptions.HTTPError as e:
-            raise Exception(f'HTTP Error {e.response}')
+        response = self._http_requests(url, method='delete', headers=headers)
 
         if response.status_code == 204:
             print(f'[{image}:{tag}] Successfully Deleted {response.status_code}')
         else:
             print(f'[{image}:{tag}] Something Wrong {response.status_code} {response.reason}')
-
-
-
-
